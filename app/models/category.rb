a5b696bed7ddffce769e442fc02895194aa3df07
James Coleman <james.k.coleman@gmail.com>
2014-06-20 09:30:27 -0400

    Complete version at end of week 4

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,7 +1,11 @@
 class Category < ActiveRecord::Base
-has_many :post_categories
-has_many :posts, through: :post_categories
+  include Sluggable
 
-validates :name, presence: true
+  has_many :post_categories
+  has_many :posts, through: :post_categories
+
+  validates :name, presence: true
+
+  sluggable_column :name
 
 end
